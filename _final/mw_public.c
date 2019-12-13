b3801a84f10119daa3463ff361841e6803130a8d
russ5 <32937206+russ5@users.noreply.github.com>
2019-12-12 22:28:16 -0500

    added comment skeleton for starSetup

--- _final/mw_public.c
+++ _final/mw_public.c
@@ -18,7 +18,15 @@ int ringSetup(int nodeId, char * configPath) {  // Include config path here?
 
 // User Function
 int starSetup(char * configPath) {
-
+    // get set of socket descriptors
+    // create a master socket on node 0
+        // setsockopt to allow multiple connections
+        // initiate setup on other machines
+    // if not node 0, create a regular socket
+    // bind socket to port
+    // if master node, connect() to sockets
+    // if regular node, accept() connection from master socket
+    // once connected, communicate as necessary
 }
 
 // User Function
