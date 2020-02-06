3049ed0b05df0e36d1e9e837a8b61f40d6dc8c62
russ5 <32937206+russ5@users.noreply.github.com>
2020-02-05 20:01:55 -0500

    repo cleanup

--- fullyConnected/include/fully_connected_connect.h
+++
@@ -1,5 +0,0 @@
-#include "config.h"
-#ifndef CAPSTONE_FULLY_CONNECTED_CONNECT_H
-#define CAPSTONE_FULLY_CONNECTED_CONNECT_H
-void initiate_connections(int node_num, int* topology, struct machine* machines);
-#endif //CAPSTONE_FULLY_CONNECTED_CONNECT_H
--- fullyConnected/src/fully_connected_test.c
+++
@@ -1,6 +0,0 @@
-#include "../include/fully_connected.h"
-
-int main(int argc, char *argv[]){
-    fully_connected(59000);
-}
-
